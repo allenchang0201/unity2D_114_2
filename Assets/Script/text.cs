ff187bbf6e843224c9bc18523060aa5f4c6e9b0f
s1315119-a11y <s1315119@taivs.tp.edu.tw>
2026-02-26 15:50:42 +0800

    Initial commit

---
+++ Assets/Script/text.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class text : MonoBehaviour
+{
+    public TMP_Text it; // Can be TextMeshProUGUI for UI elements
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
